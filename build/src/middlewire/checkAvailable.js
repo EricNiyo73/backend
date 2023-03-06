9a2756373b3b40280c8692521bb44a173a8974f7
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2023-03-06 11:08:04 +0200

    added

--- build/src/middlewire/checkAvailable.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.availables = void 0;
-const availables = async();
-exports.availables = availables;
-//# sourceMappingURL=checkAvailable.js.map
--- src/Route/dist/userRoute.dev.js
+++ src/Route/dist/userRoute.dev.js
@@ -3,16 +3,20 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports["default"] = void 0;
+
 var _express = _interopRequireDefault(require("express"));
+
 var _userController = require("../controller/userController.js");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const router = _express.default.Router();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var router = _express["default"].Router();
+
 router.post('/signup', _userController.createUser);
 router.post('/login', _userController.login);
 router.get('/', _userController.getAll);
 router.put('/:id', _userController.updateUser);
-router.delete('/:id', _userController.deleteUser);
+router["delete"]('/:id', _userController.deleteUser);
 var _default = router;
-exports.default = _default;
-//# sourceMappingURL=userRoute.js.map
+exports["default"] = _default;
